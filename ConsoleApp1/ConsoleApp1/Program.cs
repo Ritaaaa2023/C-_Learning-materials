4265128618347f1a784a875db4155131ee2e84a0
Ritaaaa2023 <jwna867@aucklanduni.ac.nz>
2024-07-03 20:32:05 +1200

    basic operation

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,143 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello,Rita, World!");
+        /*
+        //1.Variables
+
+        int x;//declaration
+        x = 123;//initialization
+
+        int y = 321;
+
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+
+        int z = x + y;
+        Console.WriteLine(z);
+
+        int age = 21;
+        Console.WriteLine("Your age is:" + age);
+
+        double height = 300.5;
+        Console.WriteLine("Your Height is " + height+"cm");
+
+        bool alive = true;
+        Console.WriteLine("Are you alive? " + alive);
+
+        char symbol = '@';
+        Console.WriteLine("Your symbol is: " + symbol);
+
+        String name = "Bro";
+        Console.WriteLine("Hello," + name);
+
+        String username = symbol + name;
+        Console.WriteLine("Your username is " + username);
+
+        //2.constants
+        const double pi = 3.14159;
+        Console.WriteLine(pi);
+
+        //3.type casting
+
+        double a = 3.14;
+        int b = Convert.ToInt32(a);
+        Console.WriteLine(b);
+
+        Console.WriteLine(a.GetType());
+
+        int c = 123;
+        double d = Convert.ToDouble(c) +0.1;
+        Console.WriteLine("d= " + d);
+
+        int e = 321;
+        string f = Convert.ToString(e);
+        Console.WriteLine("f="+f);
+
+        string g = "$";
+        char h = Convert.ToChar(g);
+        Console.WriteLine("h: " +h);
+
+        String i = "true";
+        bool j = Convert.ToBoolean(i);
+        Console.WriteLine(j);
+      
+
+
+        //4.get input value
+
+        Console.WriteLine("What's your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+
+        Console.WriteLine("What's your age? ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("You are " + age + " years old");
+          
+
+        //5.operation
+        int friends = 4;
+
+        //add
+        //friends = friends + 1;
+        //friends += 1;
+        //friends++;
+
+        //minus
+        //friends = friends - 1;
+        //friends -= 1;
+        //friends--;
+
+        //multiple
+        //friends = friends * 2;
+        //friends *= 2;
+
+        //divide
+        // friends = friends / 2;
+        //friends /= 2;
+
+        //reminder
+        int reminder = friends % 3;
+
+        Console.WriteLine(reminder);
+
+        
+
+        //6.x的2次方
+        double x = 3.99;
+        double y = 5;
+
+        double a = Math.Pow(x, 2);
+
+        //开平方
+        double b = Math.Sqrt(x);
+
+        //绝对值
+        double c = Math.Abs(x);
+
+        //保留整数
+        double d = Math.Round(x);
+
+        //向上取整
+        double e = Math.Ceiling(x);
+
+        //向下取整
+        double f = Math.Floor(x);
+
+        double g = Math.Max(x, y);
+        double h = Math.Min(x, y);
+        Console.WriteLine(h);
+
+        */
+
+        //7.generate random number
+        Random random = new Random();
+
+        // int num = random.Next(1,7) + 100;
+        double num = random.NextDouble()+1;
+
+        Console.WriteLine(num);
+             
+
     }
 }
 
